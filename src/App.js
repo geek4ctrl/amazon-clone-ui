474ec11a65d9036018828106f32d0bebe860f304
Laurent Mbuyu <LMbuyu@osiristrading.com>
2023-01-10 23:31:06 +0200

    Added global states

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from './components/Home/Home';
+import { useState } from 'react';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export const basketContext = React.createContext();
+export const user = React.createContext();
 
 function App() {
+
+  const [basket, setUserBasket] = useState([]);
+  const [userInfo, setUser] = useState({});
+
+  function total() {
+    return basket.length
+  }
+
   return (
     <Home />
-    // <h1 className="text-3xl font-bold underline">
-    //   Hello world!
-    // </h1>
   );
 }
 
